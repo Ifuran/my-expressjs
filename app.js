c333719c7d1e2915725291103a42a42d3d06a576
ifuran <silalahifran@gmail>
2023-10-08 16:50:42 +0700

    menggunakan mongodb dan mongoose library

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require("./config/mongoose");
 const express = require("express");
 const app = express();
-const productRouter = require("./app/product/routes");
-const foodRouter = require("./app/food/routes");
+// const productRouter = require("./app/product/routes");
+// const foodRouter = require("./app/food/routes");
+const productRouter2 = require("./app/product2/routes");
+const productRouter3 = require("./app/product3/routes");
 const logger = require("morgan");
 const path = require("path");
 
@@ -9,8 +12,10 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/gambar", express.static(path.join(__dirname, "uploads")));
-app.use("/api/v1", productRouter);
-app.use("/api/v2", foodRouter);
+// app.use("/api/v1", productRouter);
+// app.use("/api/v2", foodRouter);
+app.use("/api/v2", productRouter2);
+app.use("/api/v3", productRouter3);
 app.use((req, res, next) => {
   res.status(404);
   res.send({
